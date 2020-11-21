2118592f8319f1e23e085b6cd1f10c90f8bee6eb
kamada <noinci1209@gmail.com>
2020-11-22 08:04:52 +0900

    保存

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,17 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+import { Redirect } from 'react-router-dom'
 import firebase from '../config/firebase'
+import { AuthContext } from '../AuthService'
 
-const Login = () => {
+const Login = ({ history }) => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
         firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(() => {
+                history.push("/") //"/"に遷移
+            })
             .catch(err => {
                 console.log(err)
             })
     }
+    const user = useContext(AuthContext)
+
+    if (user) {
+        return <Redirect to="/" />
+    }
+
     return (
         <>
             <h1>Login</h1>
