cbb859e06d44320d23b8d3bba1ea4f9c75a38739
thao <thaopaopao@gmail.com>
2021-01-10 08:27:41 +0900

    firebase初期設定変更

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,14 +4,15 @@ import 'firebase/auth'
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDT_hdFTtH2B4K1d2iIdyjeXbYL5d7IFO4",
-    authDomain: "chatapp-451db.firebaseapp.com",
-    databaseURL: "https://chatapp-451db.firebaseio.com",
-    projectId: "chatapp-451db",
-    storageBucket: "chatapp-451db.appspot.com",
-    messagingSenderId: "440939081984",
-    appId: "1:440939081984:web:061159d54b75631fd243e5"
-}
+    apiKey: "AIzaSyD8Xha5VfTzt8WHR5pvXZ7oF7MbBXJCtuc",
+    authDomain: "chatapp-team.firebaseapp.com",
+    projectId: "chatapp-team",
+    storageBucket: "chatapp-team.appspot.com",
+    messagingSenderId: "1002553925577",
+    appId: "1:1002553925577:web:8f1ca5883e547b92dd1218",
+    measurementId: "G-1TGQV27ZY8"
+  };
+  
 
 firebase.initializeApp(firebaseConfig)
 
